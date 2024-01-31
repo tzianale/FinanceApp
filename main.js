2e848a03a0c520d0c680024428f778a15edd471a
Alexis Tziantopoulos <tzalexis.1999@gmail.com>
2024-01-31 12:23:52 +0100

    Add Tailwind CSS build script and update dependencies

--- main.js
+++ main.js
@@ -1,39 +1,37 @@
 const path = require('path');
-const {app, BrowserWindow } = require('electron');
+const {app, BrowserWindow, Menu } = require('electron');
 
 const isMac = process.platform === 'darwin';        // Check if the platform is Mac 
-const isDev = process.env.NODE_ENV !== 'production';   // Check if the app is in development mode
 
+// Create main window
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'Finance App',
-        width: isDev? 1000 : 500,
-        height: 600
+        width:  1000,
+        height: 600,
+        autoHideMenuBar: true,
 
     });
 
-    // Open dev tools if in development mode
-    /*
-    if(isDev) {
-        mainWindow.webContents.openDevTools();
-    }
-    */
-
     mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+    mainWindow.setMenu(null);
 }
 
+
+// When app is ready, create a new window
 app.whenReady().then(() => {
     createMainWindow();
 
-    app.on('activate', () => {                  // When app is activated, create a new window if there are no windows open
+    // When app is activated, create a new window if there are no windows open
+    app.on('activate', () => {                  
         if(BrowserWindow.getAllWindows().length === 0) {
             createMainWindow();
         }
     });
 });
 
-
-app.on('window-all-closed', () => {             // Quit when all windows are closed
+// Quit when all windows are closed
+app.on('window-all-closed', () => {           
     if(!isMac) {
         app.quit();
     }
